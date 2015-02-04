8244e91ee7faf22aee65b74a1f344c2064f418f7
Augustina Ragwitz <aragwitz@shutterstock.com>
2015-02-04 15:03:08 -0500

    Refactor first exercises to use mod/div and finished the rest

--- HW01.hs
+++ HW01.hs
@@ -4,41 +4,58 @@ module HW01 where
 
 -- Get the last digit from a number
 lastDigit :: Integer -> Integer
-lastDigit x = read ( [last (show x)] )
+lastDigit x = x `mod` 10
 
 -- Drop the last digit from a number
 dropLastDigit :: Integer -> Integer
-dropLastDigit x =
-  let y = show x
-  in
-    case length y of
-      1 -> 0
-      _ -> read( init y )
+dropLastDigit x = x `div` 10
 
 -- Exercise 2 -----------------------------------------
-{-
 toRevDigits :: Integer -> [Integer]
-toRevDigits x = undefined
-{-
+toRevDigits x =
+ if x < 1
+ then []
+ else
+   let
+    loop y =
+      if z == y
+      then [z]
+      else z : (loop (dropLastDigit y))
+      where z = lastDigit y
+   in loop x
+
 -- Exercise 3 -----------------------------------------
 
 -- Double every second number in a list starting on the left.
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = undefined
+doubleEveryOther [] = []
+doubleEveryOther (x:[]) = [x]
+doubleEveryOther (x:(y:zs)) = x : (y*2) : zs
 
 -- Exercise 4 -----------------------------------------
 
 -- Calculate the sum of all the digits in every Integer.
 sumDigits :: [Integer] -> Integer
-sumDigits = undefined
+sumDigits [] = 0
+sumDigits (x:xs) =
+ if lastDigit x == x
+ then x + sumDigits xs
+ else (sum (toRevDigits x)) + sumDigits xs
+
 
 
 -- Exercise 5 -----------------------------------------
 
 -- Validate a credit card number using the above functions.
 luhn :: Integer -> Bool
-luhn = undefined
+luhn x =
+  if y `mod` 10 == 0
+  then True
+  else False
+  where y = sumDigits(doubleEveryOther(toRevDigits x))
 
+
+{-
 -- Exercise 6 -----------------------------------------
 
 -- Towers of Hanoi for three pegs
