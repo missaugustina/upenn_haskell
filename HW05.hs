c83b426c7a51f6022994d391e3754f0ec3ce8e68
Augustina Ragwitz <auggy@cpan.org>
2015-04-23 22:24:32 -0400

    Add week 5 homework

---
+++ HW05.hs
@@ -0,0 +1,90 @@
+{-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE OverloadedStrings, RecordWildCards #-}
+module HW05 where
+
+import Data.ByteString.Lazy (ByteString)
+import Data.Map.Strict (Map)
+import System.Environment (getArgs)
+
+import qualified Data.ByteString.Lazy as BS
+import qualified Data.Map.Strict as Map
+
+import Parser
+import Data.Bits
+-- Exercise 1 -----------------------------------------
+
+getSecret :: FilePath -> FilePath -> IO ByteString
+getSecret x y = do
+  file1 <- BS.readFile x
+  file2 <- BS.readFile y
+  let result = BS.zipWith (xor) file1 file2
+  return $ BS.pack $ filter (/= 0) result
+
+-- Exercise 2 -----------------------------------------
+
+decryptWithKey :: ByteString -> FilePath -> IO ()
+decryptWithKey = undefined
+
+-- Exercise 3 -----------------------------------------
+
+parseFile :: FromJSON a => FilePath -> IO (Maybe a)
+parseFile = undefined
+
+-- Exercise 4 -----------------------------------------
+
+getBadTs :: FilePath -> FilePath -> IO (Maybe [Transaction])
+getBadTs = undefined
+
+-- Exercise 5 -----------------------------------------
+
+getFlow :: [Transaction] -> Map String Integer
+getFlow = undefined
+
+-- Exercise 6 -----------------------------------------
+
+getCriminal :: Map String Integer -> String
+getCriminal = undefined
+
+-- Exercise 7 -----------------------------------------
+
+undoTs :: Map String Integer -> [TId] -> [Transaction]
+undoTs = undefined
+
+-- Exercise 8 -----------------------------------------
+
+writeJSON :: ToJSON a => FilePath -> a -> IO ()
+writeJSON = undefined
+
+-- Exercise 9 -----------------------------------------
+
+doEverything :: FilePath -> FilePath -> FilePath -> FilePath -> FilePath
+             -> FilePath -> IO String
+doEverything dog1 dog2 trans vict fids out = do
+  key <- getSecret dog1 dog2
+  decryptWithKey key vict
+  mts <- getBadTs vict trans
+  case mts of
+    Nothing -> error "No Transactions"
+    Just ts -> do
+      mids <- parseFile fids
+      case mids of
+        Nothing  -> error "No ids"
+        Just ids -> do
+          let flow = getFlow ts
+          writeJSON out (undoTs flow ids)
+          return (getCriminal flow)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  crim <-
+    case args of
+      dog1:dog2:trans:vict:ids:out:_ ->
+          doEverything dog1 dog2 trans vict ids out
+      _ -> doEverything "dog-original.jpg"
+                        "dog.jpg"
+                        "transactions.json"
+                        "victims.json"
+                        "new-ids.json"
+                        "new-transactions.json"
+  putStrLn crim
